12b64a0040a2105124aea3ac38a07a81c05ead3c
sweet <daydov02@mail.ru>
2021-05-02 17:20:45 +0500

    Добавьте файлы проекта.

---
+++ weather1API/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace weather1API
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
